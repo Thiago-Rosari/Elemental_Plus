644fbb2cb0ada9cd3a717614173d11a2976ce01d
macielthiago <maciel.rosari54@gmail.com>
2025-07-04 18:57:48 -0400

    Primeiro programa em C++

---
+++ Elemental/Basic_001/Primeiro.cpp
@@ -0,0 +1,13 @@
+// Primeiro.cpp - mostra uma mensagem
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Bem vindo ao C++";
+	cout << endl;
+	cout << "Meu primeiro programa!" << endl;
+
+	return 0;
+}
